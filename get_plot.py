d4860a14aa5ddb3bc949d72589327d275a8ad725
cjb-2702 <cjamesbissett@gmail.com>
2024-08-10 01:19:49 -0700

    Basic interactivity between main plot and table

---
+++ get_plot.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+import plotly.graph_objects as go
+import plotly.io as pio
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    x1 = [i for i in range(10)]
+    y1 = x1.copy()
+    x2 = x1.copy()
+    y2 = [x*x for x in range(10)]
+    data = [
+        {"x": x1, "y": y1, "mode": "lines+markers"},
+        {"x": x2, "y": y2, "mode": "lines+markers"}
+    ]
+    layout = {
+        "title": "Line and Scatter Plot"
+    }
+    plot = {"data": data, "layout": layout}
+    table_data = [{"x": x, "y": y} for x, y in zip(x1, y1)]  # Create a list of dictionaries
+    return render_template("index.html", plot=plot, table_data=table_data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
