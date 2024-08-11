8801b64d4ea40b141b4ab68a49b365a93298ffc8
cjb-2702 <cjamesbissett@gmail.com>
2024-08-10 19:09:38 -0700

    formatting

--- get_plot.py
+++ get_plot.py
@@ -9,25 +9,30 @@ app = Flask(__name__)
 def index():
     x1 = [i for i in range(10)]
     y1 = x1.copy()
+
     x2 = x1.copy()
     y2 = [x*x for x in range(10)]
 
     #https://plotly.com/javascript/plotlyjs-function-reference/#plotlynewplot
+    plot = {"data": None, "layout": None, "config":None}
+
     #https://plotly.com/javascript/reference/
-    data = [
+    plot["data"] = [
         {"x": x1, "y": y1, "mode": "lines+markers"},
         {"x": x2, "y": y2, "mode": "lines+markers"}
     ]
 
     #https://plotly.com/javascript/reference/layout/
-    layout = {   
+    plot["layout"] = {   
         "margin":{"autoexpand": False, "t":0}
     }
+
     #https://plotly.com/javascript/configuration-options/
-    plot = {"data": data, "layout": layout, "config":{}}
+    plot["config"]={}
 
 
-    table_data = [{"x": x, "y": y} for x, y in zip(x1, y1)]
+    table_data = []#[{"x": x, "y": y} for x, y in zip(x1, y1)] #let user init
+
     return render_template("index.html", plot=plot, table_data=table_data)
 
 if __name__ == "__main__":
